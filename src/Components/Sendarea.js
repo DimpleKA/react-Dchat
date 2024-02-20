f571b40410cef929b6e3e9585ac5f2b25bc79d90
DimpleKA <153168007+DimpleKA@users.noreply.github.com>
2024-02-20 08:14:21 +0530

    vetically scrollable added successfully

--- src/Components/Sendarea.js
+++ src/Components/Sendarea.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Button, TextField, Grid, InputAdornment } from '@mui/material';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
-const Sendarea = () => {
+const SendArea = () => {
   return (
-    <div>Sendarea</div>
-  )
-}
+    <Grid container spacing={1} alignItems="center">
+      <Grid item xs={8} sm={9}>
+        <TextField fullWidth placeholder="Type your message..." variant="outlined" InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AttachFileIcon />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end">
+              <InsertEmoticonIcon />
+            </InputAdornment>
+          ),
+          sx: { borderRadius: 4 }
+        }} />
+      </Grid>
+      <Grid item xs={4} sm={3}>
+        <Button variant="contained" color="primary" fullWidth>Send</Button>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Sendarea
+export default SendArea;
